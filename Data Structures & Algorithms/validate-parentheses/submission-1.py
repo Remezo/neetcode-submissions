7018a5e1c7492aef6fc1eb50dd8b8973621c6f9a
Remezo <50808791+Remezo@users.noreply.github.com>
2026-06-17 19:56:45 -0400

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        #create a map first 
+        # for loop
+        #make sure you have teh stack is not empty and the close has a match
+        #if it's open append it
+        #return trur if there is no stack and False if there is a stack
+
+        stack=[]
+        matching={']':'[',
+             '}':'{',
+             ')':'('}
+        for c in s:
+            if c in matching:# if it's a closing character, we need to check if it already has a match in the stack
+                if stack and matching[c]==stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+
+            else:
+                stack.append(c)
+        
+        return len(stack)==0
+
+                
+        
